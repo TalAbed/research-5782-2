dbb7551de89a829a3dc8ea966620146ff8ed6599
TalAbed <47939585+TalAbed@users.noreply.github.com>
2022-03-09 00:00:11 +0200

    all in one

---
+++ mymodule.py
@@ -0,0 +1,21 @@
+"""
+Welcome to my document!
+"""
+
+def sum(x:int,y:int):
+    """
+    return the sum of two integers
+    """
+    return x+y
+
+def printHi():
+    """
+    print the word Hi
+    """
+    print("Hi")
+
+def sub(x,y):
+    """
+    return the sub of two numbers
+    """
+    return x-y
